006a91ca55afca45e25a187fd819271496a872a4
Fu Kai <ca2pa23187@gmail.com>
2021-10-02 22:45:09 +0800

    20211002

--- ds/ds.go
+++ ds/ds.go
@@ -6,21 +6,22 @@
 // 队列
 // Set
 // Map
-// 堆
-// 优先级队列
+// 堆(优先级队列)
 
 package main
 
 import (
-  "container/list"
-  "fmt"
+	"container/list"
+	"fmt"
+	dsBinHeap "github.com/emirpasic/gods/trees/binaryheap"
+	dsUtils "github.com/emirpasic/gods/utils"
 )
 
 func DsArray() {
 	nums := make([]int, 3)
 	nums[0], nums[1], nums[2] = 10, 11, 12
 
-	strs := []string{}
+	strs := [3]string{}
 	strs[0], strs[1], strs[2] = "10", "11", "12"
 
 	for i := 0; i < 5; i++ {
@@ -35,8 +36,8 @@ func DsArray() {
 	}
 	fmt.Println()
 
-	for i, v := range strs {
-		fmt.Println("idx:", i, "val:", v)
+	for _, v := range strs {
+		fmt.Println("val:", v)
 	}
 	for i, v := range nums {
 		fmt.Println("idx:", i, "val:", v)
@@ -74,9 +75,72 @@ func DsMap() {
 	fmt.Println(v0, isExist)
 }
 
+func DsHeap() {
+	// Min-heap
+	heap := dsBinHeap.NewWithIntComparator()  // empty (min-heap)
+	heap.Push(2)                              // 2
+	heap.Push(3)                              // 2, 3
+	heap.Push(1)                              // 1, 3, 2
+	fmt.Println(heap.Values())                // 1, 3, 2
+	_, _ = heap.Peek()                        // 1,true
+	_, _ = heap.Pop()                         // 1, true
+	_, _ = heap.Pop()                         // 2, true
+	_, _ = heap.Pop()                         // 3, true
+	_, _ = heap.Pop()                         // nil, false (nothing to pop)
+	heap.Push(1)                              // 1
+	heap.Clear()                              // empty
+	heap.Empty()                              // true
+	fmt.Println("size: ", heap.Size())        // 0
+	fmt.Println("not empty: ", !heap.Empty()) // 0
+
+	// Max-heap
+	inverseIntComparator := func(a, b interface{}) int {
+		return -dsUtils.IntComparator(a, b)
+	}
+	heap = dsBinHeap.NewWith(inverseIntComparator) // empty (min-heap)
+	heap.Push(2, 3, 1)                             // 3, 2, 1 (bulk optimized)
+	fmt.Println(heap.Values())                     // 3, 2, 1
+
+	// Min-heap
+	arrayHeap := dsBinHeap.NewWith(func(a, b interface{}) int {
+		// interface转array
+		return dsUtils.IntComparator(a.([]int)[2], b.([]int)[2])
+	})
+	arr0, arr1, arr2 := []int{2, 2, 2}, []int{1, 1, 1}, []int{3, 3, 3}
+	arrayHeap.Push(arr0)
+	arrayHeap.Push(arr1)
+	arrayHeap.Push(arr2)
+	fmt.Println(arrayHeap.Values())
+
+	arr, _ := arrayHeap.Pop()
+	fmt.Println(arr.([]int))
+}
 
 func main() {
-	// DsArray()
+	//DsArray()
 	//DsMap()
-	DsList()
+	//DsList()
+	DsHeap()
+	//height := [][]int{{1, 2, 3}, {1, 2, 3}}
+	//test(height)
+}
+
+func test(height [][]int) {
+
+	rLen, cLen := len(height), len(height[0])
+	fmt.Println(rLen)
+	fmt.Println(cLen)
+
+	visited := make([][]bool, rLen)
+	for i := range visited {
+		visited[i] = make([]bool, cLen)
+	}
+
+	visited[1][1] = true
+	fmt.Println(visited)
+
+	result := 0
+	result += 2
+	fmt.Println(result)
+
 }
